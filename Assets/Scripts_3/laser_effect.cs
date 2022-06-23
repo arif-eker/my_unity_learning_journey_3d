c23c47d0a8d4346f881eaa78645cdbb58b65031f
Arif Eker <eng.arifeker@gmail.com>
2022-06-23 17:12:11 +0300

    laser efekti uygulandı :)

---
+++ Assets/Scripts_3/laser_effect.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class laser_effect : MonoBehaviour
+{
+    public Transform target;
+    LineRenderer laser_line;
+    void Start()
+    {
+        laser_line = GetComponent<LineRenderer>();
+        
+    }
+
+    
+    void Update()
+    {
+        
+
+        if(Vector3.Distance(transform.position, target.position)>10f)
+        {
+            Debug.Log("Over Distance!");
+            laser_line.enabled = false;
+        }
+        else
+        {
+            laser_line.enabled = true;
+            laser_line.SetPosition(0, transform.position);
+            laser_line.SetPosition(1, target.position);
+        }
+
+    }
+}
