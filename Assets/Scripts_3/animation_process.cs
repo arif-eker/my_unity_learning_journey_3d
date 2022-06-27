d3107bc9f6672a4b0a8658f40c8a954cd6ca8283
Arif Eker <eng.arifeker@gmail.com>
2022-06-27 18:00:22 +0300

    animator işlemleri yapıldı
    herhangi bir animasyon arası geçiş işlemi klavyeden input alınarak sağlandı -trigger-bool

---
+++ Assets/Scripts_3/animation_process.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animation_process : MonoBehaviour
+{
+    Animator animationController;
+    void Start()
+    {
+        animationController = GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            animationController.SetTrigger("to_jump");
+        }
+
+        if (Input.GetKeyDown("w"))
+        {
+            animationController.SetTrigger("to_move");
+        }
+
+        if (Input.GetKeyDown("c"))
+        {
+            animationController.ResetTrigger("to_move");
+        }
+    }
+}
