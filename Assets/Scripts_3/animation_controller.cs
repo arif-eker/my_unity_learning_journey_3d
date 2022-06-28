eb357c4101d2d49477e95100d47f5196a52773eb
Arif Eker <eng.arifeker@gmail.com>
2022-06-29 01:21:07 +0300

    animator işlemleri v3
    klavyeden input ile animasyonlar oynatıldı

---
+++ Assets/Scripts_3/animation_controller.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animation_controller : MonoBehaviour
+{
+
+    Animator animationController;
+
+    void Start()
+    {
+        animationController = GetComponent<Animator>();
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            animationController.Play("walk");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            animationController.Play("jump");
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            animationController.Play("sprint");
+        }
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            animationController.Play("idle");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            animationController.Play("run");
+        }
+
+    }
+}
