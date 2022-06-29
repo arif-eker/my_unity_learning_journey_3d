8752926dc0a99dbf851539bb0593c931a9aa2567
Arif Eker <eng.arifeker@gmail.com>
2022-06-29 11:16:36 +0300

    animasyon işlemleri v4
    animasyonların bir tuşa basıldığı sürece oynatılması veya bırakınca bitmesi olayı yapıldı

--- Assets/Scripts_3/animation_controller.cs
+++ Assets/Scripts_3/animation_controller.cs
@@ -15,30 +15,50 @@ public class animation_controller : MonoBehaviour
 
     void Update()
     {
+        //if (Input.GetKeyDown(KeyCode.W))
+        //{
+        //    animationController.Play("walk");
+        //}
+
+        //The walking animation plays as long as the key is pressed
+        // idle to walk
         if (Input.GetKeyDown(KeyCode.W))
         {
-            animationController.Play("walk");
+            animationController.SetBool("to_walk",true);
         }
-
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyUp(KeyCode.W))
         {
-            animationController.Play("jump");
+            animationController.SetBool("to_walk",false);
         }
-
-        if (Input.GetKeyDown(KeyCode.LeftShift))
+        //############################################################
+        if (Input.GetKeyDown(KeyCode.Q))
         {
-            animationController.Play("sprint");
+            animationController.SetBool("to_run", true);
         }
-
-        if (Input.GetKeyDown(KeyCode.C))
+        if (Input.GetKeyUp(KeyCode.Q))
         {
-            animationController.Play("idle");
+            animationController.SetBool("to_run", false);
         }
+        //############################################################
+        //if (Input.GetKeyDown(KeyCode.Space))
+        //{
+        //    animationController.Play("jump");
+        //}
 
-        if (Input.GetKeyDown(KeyCode.Z))
-        {
-            animationController.Play("run");
-        }
+        //if (Input.GetKeyDown(KeyCode.LeftShift))
+        //{
+        //    animationController.Play("sprint");
+        //}
+
+        //if (Input.GetKeyDown(KeyCode.C))
+        //{
+        //    animationController.Play("idle");
+        //}
+
+        //if (Input.GetKeyDown(KeyCode.Z))
+        //{
+        //    animationController.Play("run");
+        //}
 
     }
 }
