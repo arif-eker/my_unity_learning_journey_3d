08d593b2edc6c1fdba19676ff7fcaf9ac7079abe
Arif Eker <eng.arifeker@gmail.com>
2022-07-01 17:22:21 +0300

    ajanların hedefe gitmesi gerçekleştirildi

---
+++ Assets/ai_process.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ai_process : MonoBehaviour
+{
+    NavMeshAgent [] agents;
+    public GameObject target;
+
+    void Start()
+    {
+        agents = GetComponentsInChildren<NavMeshAgent>();
+    }
+
+    
+    void Update()
+    {
+        foreach (var agent in agents)
+        {
+            agent.SetDestination(target.transform.position);
+        }
+    }
+}
