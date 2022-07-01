726b9f494f2507134aff8311cc53979dcc3376b2
Arif Eker <eng.arifeker@gmail.com>
2022-07-02 02:34:29 +0300

    navigasyon işlemleri v3
    iki nokta arasında devriye atma işlemi yapıldı

---
+++ Assets/Scripts_3/patrol_process.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class patrol_process : MonoBehaviour
+{
+
+    public GameObject destination_a;
+    public GameObject destination_b;
+    
+
+    NavMeshAgent agent;
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.SetDestination(destination_a.transform.position);
+    }
+
+
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "destination_a")
+        {
+            agent.SetDestination(destination_b.transform.position);
+        }
+        else
+            agent.SetDestination(destination_a.transform.position);
+    }
+
+
+
+    //string dest_name = "dest_a";
+
+    //void Start()
+    //{
+    //    agent = GetComponent<NavMeshAgent>();
+
+    //}
+
+
+    //void Update()
+    //{
+    //    if (dest_name == "dest_a")
+    //    {
+    //        agent.SetDestination(destination_a.transform.position);
+    //    }
+    //    else
+    //        agent.SetDestination(destination_b.transform.position);
+    //}
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.gameObject.name == "destination_a")
+    //    {
+    //        dest_name = "dest_b";
+    //    }
+    //    else
+    //        dest_name = "dest_a";
+    //}
+}
