85fe80e0bc6f094985185cbb5d2685d105328ce9
Arif Eker <eng.arifeker@gmail.com>
2022-07-03 11:07:36 +0300

    input işlemleri v1
     klavyeden girişler alınarak temel anlamda bir karakterin hareket etmesi sağlandı. yani A basınca sola, D basınca sağa ve zıplayınca zıplama işlemi yapılması sağlandı

---
+++ Assets/Scripts_3/input_process.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class input_process : MonoBehaviour
+{
+    float speed = 5f;
+    public GameObject myCharacter;
+
+    void Start()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        float moveH = Input.GetAxis("Horizontal");
+        float moveV = Input.GetAxis("Vertical");
+        float jump = Input.GetAxis("Jump");
+        myCharacter.transform.Translate(new Vector3 (moveH, jump,moveV) * speed * Time.deltaTime);
+
+    }
+
+    void Update()
+    {
+        
+    }
+}
