116ab4b97bea5ae05286d6a9f0c4988df699b25b
Arif Eker <eng.arifeker@gmail.com>
2022-07-05 20:52:21 +0300

    raycast işlemleri v1
    v1: bir objeden ışın gönderme işlemi yapıldı, mesafesi veya dikkate aldığı layerler denendi

---
+++ Assets/Scripts_3/raycast_process.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class raycast_process : MonoBehaviour
+{
+
+    void Start()
+    {
+
+    }
+
+    RaycastHit hit;
+    //int layerMask = 1;
+    //int layerMask = ~1;
+
+
+    private void FixedUpdate()
+    {
+        //if (Physics.Raycast(transform.position, transform.TransformDirection(new Vector3(0f, 0f, 1f)), out hit, 50,layerMask)) // 3--> layerMask
+        if (Physics.Raycast(transform.position, transform.TransformDirection(new Vector3(0f, 0f, 1f)), out hit, 50))
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(new Vector3(0f, 0f, 1f)) * hit.distance, Color.yellow);
+            //Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+
+            Debug.Log("Interaction");
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(new Vector3(0f, 0f, 1f)) * 50, Color.red);
+
+            Debug.Log("No Interaction");
+        }
+    }
+
+
+
+}
